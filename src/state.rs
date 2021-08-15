47eabff4e8f1a0e262d77a5190c55e849b92e070
solthereum <shining.superstar.1234@gmail.com>
2021-08-15 17:03:33 +0530

    Added Voter struct to the State

--- src/state.rs
+++ src/state.rs
@@ -6,8 +6,15 @@ use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+    pub voters: Vec<Voter>,
+    pub candidates: Vec<Addr>,
+    pub voted: Vec<Addr>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Voter {
+    pub address: Addr,
+    pub name: String
 }
 
 pub const STATE: Item<State> = Item::new("state");
